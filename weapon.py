0549e2391995b032baf9058046527037e3ba0700
Der <maksigeim@yandex.ru>
2026-07-19 11:27:03 +0300

    feat: add Weapon class with preset-based creation

---
+++ weapon.py
@@ -0,0 +1,22 @@
+from items import Item
+
+class Weapon(Item):
+
+    wearons = {
+        'fists': {'name': 'Кулаки', 'damage': 1, 'description': 'Обычные кулаки, которые есть почти у всех'},
+        'stick': {'name': 'Палка', 'damage': 5, 'description': 'Палка, которая раньше была частью какого то дерева, но сейчас ее можно использовать как оружие!'},
+        'sword': {'name': 'Меч', 'damage': 10, 'description': 'Древний меч'},
+    }
+
+    def __init__(self, name, description, damage):
+        
+        super().__init__(name, description)
+        
+        self.damage = damage
+
+    @classmethod
+    def from_name(cls, key):
+        data = cls.wearons[key]
+        return cls(data['name'], data['description'], data['damage'])
+    
+fists = Weapon.from_name('fists')
