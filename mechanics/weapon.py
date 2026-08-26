2c6e87623179ea00e115e63a8660491eae401778
Der <maksigeim@yandex.ru>
2026-08-26 12:43:25 +0300

    feat: add sword weapon preset

--- mechanics/weapon.py
+++ mechanics/weapon.py
@@ -4,8 +4,8 @@ class Weapon(Item):
 
     wearons = {
         'fists': {'name': 'Кулаки', 'damage': 1, 'description': 'Обычные кулаки, которые есть почти у всех'},
-        'stick': {'name': 'Палка', 'damage': 5, 'description': 'Палка, которая раньше была частью какого то дерева, но сейчас ее можно использовать как оружие!'},
-        'sword': {'name': 'Меч', 'damage': 10, 'description': 'Древний меч'},
+        'stick': {'name': 'Палка', 'damage': 3, 'description': 'Палка, которая раньше была частью какого то дерева, но сейчас ее можно использовать как оружие!'},
+        'sword': {'name': 'Меч', 'damage': 13, 'description': 'Древний меч'},
         'God_sword': {'name': 'Меч бога', 'damage': 999999, 'description': 'ема что это'},
     }
 
