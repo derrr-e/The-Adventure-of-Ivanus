2377e97515611d1250aab935c93c1decefeeba8d
Der <maksigeim@yandex.ru>
2026-09-17 08:10:39 +0300

    fix: resolve circular import issue

--- presets/items_presets.py
+++ presets/items_presets.py
@@ -1,9 +1,9 @@
-from mechanics.items import Item, Consumable
+
 
 items_presets = {
     
-    'rock': {'class': Item, 'name': 'Камень', 'description': 'Обычный камень, который можно найти на любой дороге',  'action': 'Ты не можешь использовать этот предмет', 'can_use': False},
-    'apple': {'class': Consumable, 'name': 'Яблоко', 'description': 'Сочный фрукт, который может восполнить немного здоровья', 'action': {'past': 'Съел', 'present': 'Сьесть'}, 'heal': 5, 'can_use': True} # '': {'class': Consumable, 'name': '', 'description': '', 'action': {'past': '', 'present': ''}, 'heal': , 'can_use': }  
+    'rock': {'class': "Item", 'name': 'Камень', 'description': 'Обычный камень, который можно найти на любой дороге',  'action': None, 'droppable': True, 'key': 'rock'},
+    'apple': {'class': 'Consumable', 'name': 'Яблоко', 'description': 'Сочный фрукт, который может восполнить немного здоровья', 'action': {'past': 'Съел', 'present': 'Сьесть'}, 'heal': 5, 'droppable': True, 'key': 'apple'} # '': {'class': Consumable, 'name': '', 'description': '', 'action': {'past': '', 'present': ''}, 'heal': , 'droppable': }  
     
     
     
--- presets/weapon_presets.py
+++ presets/weapon_presets.py
@@ -1,7 +1,7 @@
+
 wearons = {
-    'fists': {'name': 'Кулаки', 'damage': 1, 'description': 'Обычные кулаки, которые есть почти у всех'},
-    'stick': {'name': 'Палка', 'damage': 2, 'description': 'Палка, которая раньше была частью какого то дерева, но сейчас ее можно использовать как оружие!'},
-    'basic_sword': {'name': 'Обычный меч', 'damage': 4, 'description': 'Самый обычный меч, который когда то принадлежал одному пьянице...'},
-    'old_sword': {'name': 'Крутой меч', 'damage': 13, 'description': 'Древний меч'},
-    'God_sword': {'name': 'Меч бога', 'damage': 999999, 'description': 'ема что это'},
+    'stick': {'class': 'Weapon', 'name': 'Палка', 'damage': 2, 'description': 'Палка, которая раньше была частью какого то дерева, но сейчас ее можно использовать как оружие!', 'droppable': True},
+    'basic_sword': {'class': 'Weapon', 'name': 'Обычный меч', 'damage': 4, 'description': 'Самый обычный меч, который когда то принадлежал одному пьянице...', 'droppable': True},
+    'old_sword': {'class': 'Weapon', 'name': 'Крутой меч', 'damage': 13, 'description': 'Древний меч', 'droppable': True},
+    'God_sword': {'class': 'Weapon', 'name': 'Меч бога', 'damage': 999999, 'description': 'ема что это', 'droppable': False},
 }
