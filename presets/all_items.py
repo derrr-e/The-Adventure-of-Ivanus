e85422dea6f0511bdb5f6cbcf8dd60233772e4bd
Der <maksigeim@yandex.ru>
2026-09-17 08:01:14 +0300

    feat: add all_items dict and class_map

---
+++ presets/all_items.py
@@ -0,0 +1,4 @@
+from presets.items_presets import items_presets
+from presets.weapon_presets import wearons
+
+all_items = {**items_presets, **wearons}
---
+++ presets/class_map.py
@@ -0,0 +1,4 @@
+from mechanics.items import Item, Consumable
+from mechanics.weapon import Weapon
+
+class_map = {'Item': Item, 'Consumable': Consumable, 'Weapon': Weapon,}
